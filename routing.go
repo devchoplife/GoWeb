60a42b71d69202b8a18d6c819e59d27ae956795b
devchoplife <dritedurojaiye@gmail.com>
2021-07-24 22:42:54 +0100

    Add differnet restrictions

--- routing.go
+++ routing.go
@@ -31,4 +31,26 @@ func main() {
 	})
 
 	http.ListenAndServe(":80", r)
+
+	//ROUTING METHODS
+	//Restrict the request handler to specific methods
+	r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+	r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+	r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+	r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+	//DOMAINS AND SUBDOMAINS
+	//Restrict the request handler to specific hostnames and domains
+	r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com")
+
+	//SCHEMES
+	//Restrict the request handler to specific schemes
+	r.HandleFunc("/secure", SecureHandler).Schemes("https")
+	r.HandleFunc("/insecure", InsecureHandler).Schemes("http")
+
+	//PATH PREFIXES AND SUBROUTERS
+	//Restrict the request handler to specific path prefixes
+	bookrouter := r.PathPrefix("/books").Subrouter()
+	bookrouter.HandleFunc("/", AllBooks)
+	bookrouter.HandleFunc("/{title}", GetBook)
 }
