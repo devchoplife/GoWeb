e567656374797097a18c5ff66d759495457470a9
devchoplife <dritedurojaiye@gmail.com>
2021-07-26 18:09:00 +0100

    Implementing forms in Golang

---
+++ Forms/forms.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func main() {
+	//This shows how to simulate a simple contact form and pass the result
+	//in a struct
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		//do something with details
+		_ = details
+
+		tmpl.Execute(w, struct{ Success bool }{true})
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
